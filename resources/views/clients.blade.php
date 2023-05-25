e148048ca72a98b021196c491f79c4bab84b9fca
Navid Vakili <vakili.jd@gmail.com>
2023-05-25 11:07:45 +0330

    show pagination in client list

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -43,6 +43,9 @@
                             @endforeach
                         </tbody>
                     </table>
+                    <div class="d-flex justify-content-center">
+                {!! $clients->links( "pagination::bootstrap-4") !!}
+            </div>
                 </div>
             </div>
         </div>
