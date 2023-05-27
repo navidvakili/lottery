19dfefbb0513f55a2b42590d204dd0b2f065c9b5
Navid Vakili <vakili.jd@gmail.com>
2023-05-27 15:08:35 +0330

    enlarge dashboard form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,29 +10,37 @@
     <div class="box-body">
         <form class="row g-2" method="post" action="{{ route('execute.store') }}">
             @csrf
-            <div class="col-auto">
-                <label for="staticEmail2" class="visually-hidden">عنوان</label>
-                <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="{{ $lottery_default->title }} - {{ $lottery_default->group->members->count() }} نفر">
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">تعداد افراد شرکت کننده</label>
+                <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="{{ $lottery_default->title }} - {{ $lottery_default->group->members->count() }} نفر">
+                </div>
             </div>
-            <div class="col-2">
-                <label for="inputPassword2" class="visually-hidden">شروع شماره واحد</label>
-                <input type="number" class="form-control" name="min_vahed" min="1" required autocomplete="off" placeholder="شروع شماره واحد" style="direction:rtl">
+            <div class="form-group row" style="margin-bottom: 8px;">
+                <label for="inputPassword3" class="col-sm-2 col-form-label col-form-label-lg">شروع شماره واحد</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control form-control-lg" name="min_vahed" min="1" required autocomplete="off" placeholder="شروع شماره واحد" style="direction:rtl">
+                </div>
             </div>
-            <div class="col-2">
-                <label for="inputPassword2" class="visually-hidden">پایان شماره واحد</label>
-                <input type="number" class="form-control" name="max_vahed" min="1" required autocomplete="off" placeholder="پایان شماره واحد" style="direction:rtl">
+            <div class="form-group row" style="margin-bottom: 8px;">
+                <label for="inputPassword3" class="col-sm-2 col-form-label col-form-label-lg">پایان شماره واحد</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control form-control-lg" name="max_vahed" min="1" required autocomplete="off" placeholder="پایان شماره واحد" style="direction:rtl">
+                </div>
             </div>
-            <div class="col-3">
-                <label for="inputPassword3" class="visually-hidden">تعداد افراد</label>
-                <input type="number" class="form-control" name="num_peoples" min="1" max="{{ $lottery_default->group->members->count() }}" required autocomplete="off" style="direction:rtl" placeholder="تعداد افراد برای قرعه کشی">
+            <div class="form-group row" style="margin-bottom: 8px;">
+                <label for="inputPassword3" class="col-sm-2 col-form-label col-form-label-lg">تعداد افراد</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control form-control-lg" name="num_peoples" min="1" max="{{ $lottery_default->group->members->count() }}" required autocomplete="off" style="direction:rtl" placeholder="تعداد افراد برای قرعه کشی">
+                </div>
             </div>
-
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary mb-3">اجرای قرعه کشی</button>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary mb-3 btn-lg">اجرای قرعه کشی</button>
             </div>
         </form>
-
+        @if($lottery_members->count() > 0)
         <div class="table-responsive">
+            <a href="{{ route('excel.export', $lottery_default->id) }}" class="btn btn-info">خروجی اکسل</a>
             <table class="table table-striped">
                 <thead>
                     <tr>
@@ -59,6 +67,7 @@
                 {!! $lottery_members->links( "pagination::bootstrap-4") !!}
             </div>
         </div>
+        @endif
     </div>
 </div>
 @endif
